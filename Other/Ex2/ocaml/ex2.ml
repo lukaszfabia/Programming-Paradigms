3419be7bf7319b3c1ad5605373e862c37c6e9067
LukaszAcer <ufabia03@gmail.com>
2023-09-05 14:42:34 +0200

    ex2

---
+++ Other/Ex2/ocaml/ex2.ml
@@ -0,0 +1,65 @@
+let rec fib x =
+  match x with
+  | 0 -> 0
+  | 1 -> 1
+  | _ -> fib (x - 1) + fib (x - 2)
+
+
+let rec fibTail x =
+  let rec fibTailHelper (x, prev, prevPrev) =
+    match x with
+    | 0-> prevPrev
+    | 1-> prev
+    | _ -> fibTailHelper (x - 1, prev + prevPrev, prev)
+  in fibTailHelper (x, 1, 0)
+
+
+  let rec root3 number =
+    let rec root3Helper (number, guess) =
+      if abs_float (guess ** 3.0 -. number) <= (10.0 ** (-15.0)) *. abs_float number then
+        guess
+      else
+        root3Helper (number, guess +. (number /. (guess ** 2.0) -. guess) /. 3.0)
+    in
+    root3Helper (number, 1.0) 
+  
+let initSegment (l1,l2) =
+  let rec initSegmentHelper (l1,l2) =
+    match (l1,l2) with
+    | ([],_) -> true
+    | (_,[]) -> false
+    | (h1::t1,h2::t2) -> if h1 == h2 then initSegmentHelper (t1,t2) else false
+  in
+  initSegmentHelper (l1,l2)
+
+
+let rec replaceNth (list, index, element) =
+  match (list, index) with
+  | ([],_) -> []
+  | (_ :: tail, 0) -> element :: tail
+  | (head :: tail, _) -> head :: replaceNth (tail, index - 1, element)
+  
+
+let list = [0;1;2;3;4;5;6;7;8;9;10]
+
+let check = replaceNth (list, 5, -100)
+let check2 = replaceNth (list, 0, -100)
+
+let check3 = initSegment (list, list)
+let check4 = initSegment (list, [0;1;2;3;4;5;6;7;8;9]) == false
+let check5 = initSegment([0;1;2;3;4;5;6;7;8;9], list)
+
+let check6 = root3 (27.0) <=  10.0 **(-15.0) *. 3.0
+let check7 = root3 (8.0) <= 10.0 **(-15.0) *. 2.0
+let check8 = root3 (1000.0) <= 10.0 **(-15.0) *. 10.0
+let check9 = root3 (0.625) <= 10.0 **(-15.0) *. 0.5
+let check10 = root3 (0.0) <= 10.0 **(-15.0) *. 0.0
+
+
+let check11 = fib(0) == fibTail(0) && fib(0) == 0
+let check12 = fib(1) == fibTail(1) && fib(1) == 1
+let check13 = fib(2) == fibTail(2) && fib(2) == 1
+let check14 = fib(3) == fibTail(3) && fib(3) == 2
+let check15 = fib(4) == fibTail(4) && fib(4) == 3
+
+
---
+++ Other/Ex2/scala/ex2.scala
@@ -0,0 +1,73 @@
+import scala.annotation.tailrec
+
+// 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+def fib(x: Int) : Int = {
+    x match {
+        case 0 => 0
+        case 1 => 1
+        case _ => fib(x-1) + fib(x-2)
+    }
+}
+
+def fibTail(x: Int) : Int  = {
+    @tailrec
+    def getFib(x: Int, prev: Int, prevPrev: Int): Int = {
+        if (x == 0) 0
+        else if (x == 1) prev
+        else getFib(x-1, prev + prevPrev, prev)
+    }
+    getFib(x, 1, 0)
+}
+
+val list = (0 to 10).toList
+val fibList = list.map(fib)
+val fibTailList = list.map(fibTail)
+
+println(fibList==fibTailList)
+
+def root3(number: Double) : Double = {
+    @tailrec
+    def calculate(x: Double): Double = {
+        val next = x - (x*x*x - number)/(3*x*x)
+        if (next == x) x
+        else calculate(next)
+    }
+    calculate(number)
+}
+
+
+println(root3(0.2))
+
+//czy xs jest w ys
+def initSegment[A](xs: List[A], ys: List[A]): Boolean = {
+    if (xs.length > ys.length){
+        false
+    }else{
+        (xs, ys) match{
+            case (Nil, _) => true
+            case (_, Nil) => false
+            case _ => 
+                if (ys.head == xs.head){
+                    initSegment(xs.tail, ys.tail)
+                }else{
+                    false
+                }
+        }
+    }
+}
+
+println(initSegment(Nil, Nil))
+println(initSegment(List(1,2,3), List(1,2,3)))
+println(!initSegment(List('a', 'b', 'c'), List('b', 'c')))
+
+
+def replaceNth[A](xs: List[A], index: Int, element: A): List[A] = {
+    (xs, index) match{
+        case (Nil, _) => Nil
+        case (_ :: tail, 0) => element::tail
+        case (head :: tail, _) => head :: replaceNth(tail, index-1, element)
+    }
+}
+
+println(replaceNth(List(1,2,3,4,5), 0, 99))
+println(replaceNth(List(1,2,3,4,5), 2, 99))
