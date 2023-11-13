1d08efd346829b12f320894c8aceae54511f3936
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-11-13 17:47:48 +0100

    poprawienie czytelnosci i test

--- Laboratory/Lab3/scala/list3.scala
+++ Laboratory/Lab3/scala/list3.scala
@@ -1,11 +1,22 @@
 def composite(n: Int): List[Int] = {
+  def divisorList(element: Int): List[Int] = {
+    for {
+      divisor <- List.range(2, Math.sqrt(element).toInt + 1) if element % divisor == 0
+    } yield divisor
+  }
+
   for {
     element <- List.range(2, n + 1)
-    if (for {
-      divisor <- List.range(2, Math.sqrt(element).toInt + 1) if element % divisor == 0
-    } yield divisor) != Nil // sprawdzenei czy lista dzielnikow nie jest pusta jak jest to liczba pierwsza
+    if divisorList(element) != Nil // sprawdzenei czy lista dzielnikow nie jest pusta jak jest to liczba pierwsza
   } yield element
 }
 
-List.range(-1, 11).foreach(x => println(composite(x)))
+def printRangeLists(begin : Int, end : Int): Unit = {
+  for {
+    element <- List.range(begin, end + 1)
+  } yield {
+    println(composite(element))
+  }
+}
 
+printRangeLists(1, 20)
