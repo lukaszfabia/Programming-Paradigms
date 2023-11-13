f2f7b9c91cd2be9f27c126eaed48f2f66e9672e3
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-11-13 22:11:32 +0100

    Update list3.ml

--- Laboratory/Lab3/ocaml/list3.ml
+++ Laboratory/Lab3/ocaml/list3.ml
@@ -1,12 +1,20 @@
-let (><) list1 list2 = 
- fun f ->
+(* obie wersje maja rownowazne sobie sygnatury *)
+
+(* let (><) list1 list2 f = 
     let rec aux lst1 lst2 = 
       match lst1, lst2 with
       | [], _ -> []
       | _, [] -> []
       | x1 :: xTail, y1 :: yTail -> f x1 y1 :: aux xTail yTail
     in
-    aux list1 list2;;
+    aux list1 list2;; *)
+
+let rec (><) list1 list2 f = 
+ match list1, list2 with  
+ | [], _ -> []
+ | _, [] -> []
+ | head1 :: tail1, head2 :: tail2 -> f head1 head2 :: (tail1 >< tail2) f
+;;
 
 let test1 = ([1;2;3] >< [4;5;6]) (fun x y -> x + y);;
 let test2 = ([] >< [1;212;431]) (fun x y -> x + y);;
@@ -22,6 +30,3 @@ let test8 = ([1;2;3] >< [4;5;6;8]) (fun x y -> x * y);;
 let test9 = ([1;12;3;0;1] >< [32;42]) (fun x y -> y - x);;
 
 let modyfikacja_z_listy_1 = ([3;1;65;3;1] >< [1;8;4;12;76;2]) (fun x y -> if x > y then x else y);; 
-
-
-
