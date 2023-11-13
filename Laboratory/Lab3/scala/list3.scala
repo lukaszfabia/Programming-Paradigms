5eb59bc32b90321fff465b30b7c73075a93fb47c
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-11-13 17:36:19 +0100

    jeszce poprawka zeby bylo tylko listrange i for

--- Laboratory/Lab3/scala/list3.scala
+++ Laboratory/Lab3/scala/list3.scala
@@ -2,9 +2,10 @@ def composite(n: Int): List[Int] = {
   for {
     element <- List.range(2, n + 1)
     if (for {
-      divisor <- List.range(2, element) if element % divisor == 0
-    } yield divisor) != Nil // sprawdzenei czy lista dzielnikow nie jest pusta jak jest to liczba pierwsza 
+      divisor <- List.range(2, Math.sqrt(element).toInt + 1) if element % divisor == 0
+    } yield divisor) != Nil // sprawdzenei czy lista dzielnikow nie jest pusta jak jest to liczba pierwsza
   } yield element
 }
 
 List.range(-1, 11).foreach(x => println(composite(x)))
+
