d06b157099b9e152c01b81b0f7e7cd535397af73
Lukasz <ufabia03@gmail.com>
2023-11-13 15:15:22 +0100

    teraz jest poprawnie chyba

--- Laboratory/Lab3/scala/list3.scala
+++ Laboratory/Lab3/scala/list3.scala
@@ -1,11 +1,8 @@
-def composite(n :Int): List[Int] = {
-    def isComposite(i: Int): Boolean = {
-        var res: Boolean = false
-        for (j <- 2 to Math.sqrt(i).toInt; if i % j == 0) res = true
-        res 
-    }
-
-    for (x <- List.range(2, n + 1); if isComposite(x)) yield x
+def composite(n: Int): List[Int] = {
+  for {
+    x <- List.range(2, n + 1)
+    j <- 2 to Math.sqrt(x).toInt
+    if x % j ==0
+  } yield x
 }
-
-List.range(2, 11).foreach(x=> println(composite(x)))
+List.range(2, 11).foreach(x => println(composite(x)))
