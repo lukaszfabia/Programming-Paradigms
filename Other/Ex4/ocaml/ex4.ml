a2f94bd0e56e6ffb2a719772cccba9ec714b8422
Lukasz <ufabia03@gmail.com>
2023-11-23 20:20:42 +0100

    poprawki do samego ide

--- Other/Ex4/ocaml/ex4.ml
+++ Other/Ex4/ocaml/ex4.ml
@@ -1,5 +1,6 @@
-let f1 x y z = x y z;; 
+let f1 x y z = x y z;;
 (* val f1 : ('a -> 'b -> 'c) -> 'a -> 'b -> 'c = <fun> *)
+
 let f2 x y = function z -> x::y;;
 (* val f2 : 'a -> 'a list -> 'b -> 'a list = <fun> *)
 
