8995409f229328af118cbadcb2808da47970f657
LukaszAcer <ufabia03@gmail.com>
2024-02-03 16:34:38 +0100

    poprawki do zd

--- Other/Ex10/ex10.scala
+++ Other/Ex10/ex10.scala
@@ -12,25 +12,29 @@ def copy[A](dest: mutable.Seq[A], src: mutable.Seq[A]): Unit = {
   else throw new IllegalArgumentException("lengths of dest and src are not equal")
 }
 
-val dest = mutable.Seq(1, 2, 3)
-val src = mutable.Seq(4, 5, 6)
-copy(dest, src)
-println("result of dest: " + dest)
-
 //zad 2
 
 abstract class Sequence[+A]:
-    def append[A](x: Sequence[A]): Sequence[A]
-// przyczyna bledu jest taka ze funkcja nie miala 
-// zdefiniowanego odp typu chodzi o append[A] <- 
+    def append[A](x: Sequence[A]): Sequence[A] // nalezy podac typ jaki jest w parametrze funckji z A a A jest kowariantne 
 
 
 // zad 1 
 
-class GenericCellImm[T] (val x: T)
+class GenericCellMut1[+T] (val x: T) // nalezy zmienic var na val bo problem byl z mutatorem bo 
+// moznaby bylo przypisac różne instancje danego typu do zmiennej
 
-class GenericCellImm[+T] (val x: T)
+// class GenericCellMut[-T] (private var x: T)
 
-class GenericCellMut[T] (var x: T)
+class GenericCellMut2[-T, A >: T] (private var x: A) // nalezy zagwarantowac ze A jest nadtypem T i
+//  dziala, czyli typ A jest bardziej ogolny 
 
-class GenericCellMut[+T] (val x: T)
+
+
+
+@main
+def main(): Unit ={
+  val dest = mutable.Seq(1, 2, 3)
+  val src = mutable.Seq(4, 5, 6)
+  copy(dest, src)
+  println("result of dest: " + dest)
+}
